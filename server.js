08d876edeff9d19944d26c0630b0fe542f356c9a
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-26 14:39:18 -0500

    Tabs Test

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {
-    DATABASE,
-    PORT
+	DATABASE,
+	PORT
 } = require('./config');
 
 const app = express();
@@ -16,33 +16,37 @@ app.use(bodyParser.json());
 // ADD ENDPOINTS HERE
 
 app.post('/api/stories', (req, res) => {
-    const requiredFields = ['title', 'url'];
-    for (let i = 0; i < requiredFields.length; i++) {
-        const field = requiredFields[i];
-        if (!(field in req.body)) {
-            const message = `Missing ${field} in request body`;
-            console.log(message);
-            return res.status(400).send(message);
-        }
-    }
-    knex('article')
-        .insert({
-            title: req.body.title,
-            url: req.body.url
-        }).then(() => {
-            const message = `Your post titled ${req.body.title} can be found at ${req.body.url}`;
-            res.status(201).json(message);
-        });
+	const requiredFields = ['title', 'url'];
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing ${field} in request body`;
+			console.log(message);
+			return res.status(400)
+				.send(message);
+		}
+	}
+	knex('article')
+		.insert({
+			title: req.body.title,
+			url: req.body.url
+		})
+		.then(() => {
+			const message = `Your post titled ${req.body.title} can be found at ${req.body.url}`;
+			res.status(201)
+				.json(message);
+		});
 });
 
 app.get('/api/stories', (req, res) => {
-    knex.select('title', 'url', 'votes')
-        .from('article')
-        .orderBy('votes', 'desc')
-        .limit(20)
-        .then((results) => {
-            res.status(200).json(results);
-        });
+	knex.select('title', 'url', 'votes')
+		.from('article')
+		.orderBy('votes', 'desc')
+		.limit(20)
+		.then((results) => {
+			res.status(200)
+				.json(results);
+		});
 
 })
 
@@ -50,43 +54,45 @@ let server;
 let knex;
 
 function runServer(database = DATABASE, port = PORT) {
-    return new Promise((resolve, reject) => {
-        try {
-            knex = require('knex')(database);
-            server = app.listen(port, () => {
-                console.info(`App listening on port ${server.address().port}`);
-                resolve();
-            });
-        } catch (err) {
-            console.error(`Can't start server: ${err}`);
-            reject(err);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			knex = require('knex')(database);
+			server = app.listen(port, () => {
+				console.info(`App listening on port ${server.address().port}`);
+				resolve();
+			});
+		} catch (err) {
+			console.error(`Can't start server: ${err}`);
+			reject(err);
+		}
+	});
 }
 
 function closeServer() {
-    return knex.destroy().then(() => {
-        return new Promise((resolve, reject) => {
-            console.log('Closing servers');
-            server.close(err => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve();
-            });
-        });
-    });
+	return knex.destroy()
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('Closing servers');
+				server.close(err => {
+					if (err) {
+						return reject(err);
+					}
+					resolve();
+				});
+			});
+		});
 }
 
 if (require.main === module) {
-    runServer().catch(err => {
-        console.error(`Can't start server: ${err}`);
-        throw err;
-    });
+	runServer()
+		.catch(err => {
+			console.error(`Can't start server: ${err}`);
+			throw err;
+		});
 }
 
 module.exports = {
-    app,
-    runServer,
-    closeServer
+	app,
+	runServer,
+	closeServer
 };
