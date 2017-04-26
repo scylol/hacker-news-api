56507ed29abd58be6012ca5210eda48a2f15e886
Scylol <djd3dobbin@gmail.com>
2017-04-26 15:06:02 -0400

    added post endpoint

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
-const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/hacker-news-api';
-
+// const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
+// 'postgresql://dev:dev@localhost/hacker-news-api'
+require('dotenv').config();
+const {DATABASE_URL} = process.env;
 exports.DATABASE = {
-  client: 'pg',
-  connection: DATABASE_URL,
-  debug: true
+	client: 'pg',
+	connection: DATABASE_URL,
+	debug: false
 };
 
 exports.PORT = process.env.PORT || 8080; 
+
+console.log(DATABASE_URL);
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 
 const { DATABASE, PORT } = require('./config');
 
+// const knex = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     database: DATABASE_URL
+//   },
+//   debug: true
+// });
+
 const app = express();
 
 app.use(morgan(':method :url :res[location] :status'));
@@ -12,43 +20,63 @@ app.use(bodyParser.json());
 
 // ADD ENDPOINTS HERE
 
+app.post('/api/stories',(req, res) => {
+	const requiredFields = ['title', 'url'];
+	for (let i = 0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if(!(field in req.body)) {
+			const message = `Missing ${field} in request body`;
+			console.log(message);
+			return res.status(400).send(message);
+		}
+	}
+	knex('article')
+    .insert({title: req.body.title, url: req.body.url}).then(() => {
+	    const message = `Your post titled ${req.body.title} can be found at ${req.body.url}`;
+	    res.status(201).json(message);
+});
+	
+});
+
+
+
 let server;
 let knex;
 function runServer(database = DATABASE, port = PORT) {
-return new Promise((resolve, reject) => {
-	try {
-	knex = require('knex')(database);
-	server = app.listen(port, () => {
-	console.info(`App listening on port ${server.address().port}`);
-	resolve();
-});
-}
-	catch (err) {
-	console.error(`Can't start server: ${err}`);
-	reject(err);
-}
-});
+	return new Promise((resolve, reject) => {
+		try {
+			knex = require('knex')(database);
+			server = app.listen(port, () => {
+				console.info(`App listening on port ${server.address().port}`);
+				resolve();
+			});
+		}
+		catch (err) {
+			console.error(`Can't start server: ${err}`);
+			reject(err);
+		}
+	});
 }
 
 function closeServer() {
-return knex.destroy().then(() => {
-return new Promise((resolve, reject) => {
-	console.log('Closing servers');
-	server.close(err => {
-	if (err) {
-	return reject(err);
-}
-	resolve();
-});
-});
-});
+	return knex.destroy().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing servers');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
 }
 
 if (require.main === module) {
 	runServer().catch(err => {
-	console.error(`Can't start server: ${err}`);
-	throw err;
-});
+		console.error(`Can't start server: ${err}`);
+		throw err;
+	});
 }
 
 module.exports = { app, runServer, closeServer };
